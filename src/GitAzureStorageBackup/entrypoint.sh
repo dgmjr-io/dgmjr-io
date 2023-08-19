c3d869c94ab60e8680b6112c842ec011536c6fbd
David G. Moore, Jr <david@dgmjr.io>
2023-08-18 22:50:23 -0400

    UPDATED VSCode settings, and fixed an issue in entrypoint.sh.
    - Updated Rule Settings in .config
    - Updated VSCode settings in .vscode/settings.json
    - Fixed an issue in entrypoint.sh where the modified date was incorrect
    - Updated subproject commit in src/Types

--- src/GitAzureStorageBackup/entrypoint.sh
+++ src/GitAzureStorageBackup/entrypoint.sh
@@ -1,14 +1,16 @@
-# 
+#!/bin/sh
+
+#
 # entrypoint.sh
-# 
+#
 #   Created: 2023-06-09-10:15:39
 #   Modified: 2023-06-09-10:15:40
-# 
+#
 #   Author: David G. Moore, Jr. <david@dgmjr.io>
-#   
+#
 #   Copyright © 2022 - 2023 David G. Moore, Jr., All Rights Reserved
 #      License: MIT (https://opensource.org/licenses/MIT)
-# 
+#
 
 git config --global user.email "$GIT_USEREMAIL"
 git config --global user.name "$GIT_USERNAME"
